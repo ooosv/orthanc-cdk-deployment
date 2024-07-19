d35dd34ac93c88c4181a55fb985f240742d48426
EC2 Default User <ec2-user@ip-172-31-25-148.eu-central-1.compute.internal>
2024-07-19 07:28:24 +0000

    Infrastructure setup.

--- infrastructure/lib/local-image-official-s3/custom-script.sh
+++ infrastructure/lib/local-image-official-s3/custom-script.sh
@@ -4,4 +4,4 @@ set -o errexit
 set -o xtrace
 
 # Removing S3 plugin so it won't crash Orthanc at startup, if the configuration is not present
-rm -f /usr/share/orthanc/plugins/libOrthancAwsS3Storage.so
+# rm -f /usr/share/orthanc/plugins/libOrthancAwsS3Storage.so
